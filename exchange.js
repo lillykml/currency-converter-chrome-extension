93b65805d40c9d4acc5948650f085bbf8563edb2
lillykml <64362067+lillykml@users.noreply.github.com>
2023-03-19 12:43:15 +0100

    Chose more advanced API

--- exchange.js
+++ exchange.js
@@ -1,28 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
     const amount = document.getElementById("amount");
-    const currency = document.getElementById("currency");
+    const originCurrency = document.getElementById("originCurrency");
+    const targetCurrency = document.getElementById("targetCurrency");
     const convert = document.getElementById("convert");
     const result = document.getElementById("result");
 
     const apiKey= "MdANcUS3Q4meavVL2BqEbQ==jYijBPbvHQrCi72W";
-    const apiUrl = "https://api.api-ninjas.com/v1/exchangerate?pair=USD_";
 
     if (convert) {
         convert.addEventListener('click', () => {
             const amountTotal = amount.value;
-            const currencyTotal = currency.value;
-            const url = apiUrl + currencyTotal;
+            const origin = originCurrency.value;
+            const target = targetCurrency.value;
+            const url = `https://api.api-ninjas.com/v1/convertcurrency?have=${origin}&want=${target}&amount=${amountTotal}`;
         
             fetch(url, {
                 headers: {
                     "X-API-KEY": apiKey
                 }
             })
-            .then(response=>response.json())
+            .then(
+                response => response.json()
+            )
             .then(data => {
-                const rate = data.exchange_rate;
-                const resultValue = amountTotal * rate;
-                result.innerHTML = `${amountTotal} ${currencyTotal} = ${resultValue.toFixed(2)} USD`;
+                console.log(data.new_amount);
+                result.innerHTML = `${amountTotal} ${origin} = ${data.new_amount} ${target}`;
             })
             .catch(error => {
                 console.error("Request failed:", error);
